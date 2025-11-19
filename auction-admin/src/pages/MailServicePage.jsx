9320ef201e70f28336bab77014b471ff76b648d4
Ezza Wan <ezzawan9@gmail.com>
2025-11-19 13:33:10 +0100

    Add back action to mail service page

--- auction-admin/src/pages/MailServicePage.jsx
+++ auction-admin/src/pages/MailServicePage.jsx
@@ -1,9 +1,20 @@
 import { Page } from '@shopify/polaris';
+import { useLocation, useNavigate } from 'react-router-dom';
 import MailServiceSettings from '../components/MailServiceSettings';
 
 export default function MailServicePage() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const query = location.search || '';
+
   return (
-    <Page title="Mail Service">
+    <Page
+      title="Mail Service"
+      backAction={{
+        content: 'Back',
+        onAction: () => navigate(`/${query}`),
+      }}
+    >
       <MailServiceSettings />
     </Page>
   );
