bc73888eb61c8daea703930d3b95ba83cadaf45f
Ezza Wan <ezzawan9@gmail.com>
2026-01-05 10:42:00 +0100

    Render Bidly dashboard in embedded app

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -95,10 +95,6 @@ export default function App() {
 
   return (
     <AppProvider embedded apiKey={apiKey} forceRedirect>
-      <s-app-nav>
-        <s-link href="/app">Home</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
-      </s-app-nav>
       <Outlet />
     </AppProvider>
   );
